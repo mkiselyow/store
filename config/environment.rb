4f31fef5121b1ed73d321392f7de498ceb5825c1
MaximKiselyow <m.kiselyow@gmail.com>
2017-07-17 11:17:06 +0300

    add_devise_gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Heroku & Devise production
+# config.action_mailer.default_url_options = { :host => 'localhost' }
