96d2ef3aff762adfe34a6450d1bab0c89cdc2a6c
MaximKiselyow <m.kiselyow@gmail.com>
2017-08-09 08:55:37 +0300

    same_config_of_cloudinary_for_each_env

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -6,9 +6,14 @@ Cloudinary.config do |config|
     config.enhance_image_tag = true
     config.static_image_support = false
   elsif Rails.env.production?
-    config.cloud_name = "hfy83q30e"
-    config.api_key = '994467127933574'
-    config.api_secret = '3fON5atyTpl9uUGZVHXqlFjh-jQ'
+    # config.cloud_name = "hfy83q30e"
+    # config.api_key = '994467127933574'
+    # config.api_secret = '3fON5atyTpl9uUGZVHXqlFjh-jQ'
+    # config.enhance_image_tag = true
+    # config.static_image_support = false
+    config.cloud_name = "dapc2mxjl"
+    config.api_key = '816189238925946'
+    config.api_secret = 'xQs9nIUulMx6QNkGmzQOdhVg2_s'
     config.enhance_image_tag = true
     config.static_image_support = false
   elsif Rails.env.test?
