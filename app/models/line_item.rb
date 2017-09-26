02ef26e55d12672eb611e1d595c4d61f1d9c65f6
MaximKiselyow <m.kiselyow@gmail.com>
2017-09-26 23:15:38 +0300

    fix with nilclass

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,7 +3,7 @@ class LineItem < ApplicationRecord
   belongs_to :cart
 
   def count_line_item_price
-    if self.product.price.class != nil
+    if self.product.price != nil
       self.product.price * self.quantity
     end
   end
