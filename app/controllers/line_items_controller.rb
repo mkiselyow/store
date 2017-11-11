02ad9750608f076e315c0aa22ffd231d4b5f668e
CrashGtaFan <gtafan51@gmail.com>
2017-11-11 17:43:11 +0200

    Update view of cart page. Add zoom picture of product

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,26 +3,14 @@ class LineItemsController < ApplicationController
   # before_action :set_cart, only: [:create]
   before_action :set_line_item, only: %i[show edit update destroy]
 
-  # GET /line_items
-  # GET /line_items.json
   def index
-    @line_items = LineItem.all
+    @line_items = LineItem.order(:id)
   end
 
-  # GET /line_items/1
-  # GET /line_items/1.json
-  def show; end
-
-  # GET /line_items/new
   def new
     @line_item = LineItem.new
   end
 
-  # GET /line_items/1/edit
-  def edit; end
-
-  # POST /line_items
-  # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
@@ -40,8 +28,6 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /line_items/1
-  # PATCH/PUT /line_items/1.json
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
@@ -54,8 +40,6 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # DELETE /line_items/1
-  # DELETE /line_items/1.json
   def destroy
     @line_item.destroy
     respond_to do |format|
@@ -70,13 +54,14 @@ class LineItemsController < ApplicationController
       decreased_quantity = @line_item.quantity - 1
       @line_item.update(quantity: decreased_quantity)
       respond_to do |format|
-        format.html { redirect_to root_url, notice: 'Количество было успешно уменьшено' }
+        format.html { redirect_to cart_path(@line_item.cart_id), notice: 'Количество было успешно уменьшено' }
         format.json { head :no_content }
       end
     else
       @line_item.destroy
+      # redirect_to :back, notice: 'Товар был успешно убран из корзины'
       respond_to do |format|
-        format.html { redirect_to root_url, notice: 'Товар был успешно убран из корзины' }
+        format.html { redirect_to cart_path(@line_item.cart_id), notice: 'Товар был успешно убран из корзины' }
         format.json { head :no_content }
       end
     end
@@ -88,11 +73,11 @@ class LineItemsController < ApplicationController
       increased_quantity = @line_item.quantity + 1
       @line_item.update(quantity: increased_quantity)
       respond_to do |format|
-        format.html { redirect_to root_url, notice: 'Количество было успешно уменьшено' }
+        format.html { redirect_to cart_path(@line_item.cart_id), notice: 'Количество было успешно уменьшено' }
         format.json { head :no_content }
       end
     else
-      format.html { redirect_to root_path, notice: 'Неизвестная ошибка' }
+      format.html { redirect_to cart_path(@line_item.cart_id), notice: 'Неизвестная ошибка' }
     end
   end
 
