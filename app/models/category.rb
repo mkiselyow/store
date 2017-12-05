c9fc203eed3b2639b7a1bc6f57c18594b3b43a5b
CrashGtaFan <gtafan51@gmail.com>
2017-12-05 18:15:01 +0200

    Add paymant page and category tree

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,9 +6,6 @@ class Category < ApplicationRecord
   has_many :subcategories, class_name: 'Category',
                            foreign_key: :parent,
                            dependent: :destroy
-  # has_many :subsubcategories, class_name: 'Category',
-  #                          foreign_key: :parent_id,
-  #                          dependent: :destroy
 
   has_one :parent, class_name: 'Category',
                    primary_key: :parent_id, foreign_key: :id
@@ -16,7 +13,7 @@ class Category < ApplicationRecord
   accepts_nested_attributes_for :subcategories, allow_destroy: true
   # accepts_nested_attributes_for :subsubcategories, allow_destroy: true
 
-  scope :without_parent, -> { where(parent_category_id: nil).where(parent_subcategory_id: nil) }
+  scope :only_parent, -> { where(ancestry: nil) }
   scope :without_subcategory, -> { where('parent_category_id IS NOT NULL') }
 
   def self.get_collection_of_categories_ids
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,7 @@ Rails.application.routes.draw do
     resources :line_items
   end
   resources :shares, only: [:show, :index]
-  resources :categories do
-    resources :subcategories, only: [:show]
-  end
+  resources :categories
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :users
   resources :comments
