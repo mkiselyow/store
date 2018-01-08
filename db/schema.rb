0c8af834636202ad6b37de94965a8de483fc357c
MaximKiselyow <m.kiselyow@gmail.com>
2018-01-08 23:29:12 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105083237) do
+ActiveRecord::Schema.define(version: 20171228135630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -172,6 +172,7 @@ ActiveRecord::Schema.define(version: 20180105083237) do
     t.boolean "material_another"
     t.boolean "material_wooden"
     t.boolean "material_fabric"
+    t.boolean "material_mixed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
