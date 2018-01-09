050965e1c49511c0b5cc9a2c4d9235b88cb97004
MaximKiselyow <m.kiselyow@gmail.com>
2018-01-09 03:56:05 +0200

    action delete products created today

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Rails.application.routes.draw do
     resources :useful_articles
     resources :products do
       collection { post :import }
-      delete '/admin/products' => 'admin_products#delete_products_created_today'
+      delete :delete_products_created_today, on: :collection
     end
     resources :categories do
       collection { post :sort }
