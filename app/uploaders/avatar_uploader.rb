6d203878cef7abc69bc37b2496c5cd98ba1a453c
CrashGtaFan <gtafan51@gmail.com>
2018-01-23 13:21:07 +0200

    Change Cloudinary to Minimagick

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,34 +1,27 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
+  include CarrierWave::MiniMagick
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   def default_url
     [thumb, 'default_user.png'].compact.join('_')
   end
 
-  process tags: ['photo_album_sample']
-  process convert: 'jpg'
-
   version :thumb do
-    eager
     resize_to_fit(200, 200)
-    cloudinary_transformation quality: 80
   end
 
   version :small do
-    eager
     resize_to_fit(50, 50)
-    cloudinary_transformation quality: 80
   end
 
   version :medium do
-    eager
     resize_to_fit(400, 400)
-    cloudinary_transformation quality: 80
   end
 
   version :large do
-    eager
     resize_to_fit(800, 800)
-    cloudinary_transformation quality: 80
   end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
+  include CarrierWave::MiniMagick
 
-  process convert: 'png'
-  process tags: ['post_picture']
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   def default_url
     [thumb, 'default_image.png'].compact.join('_')
@@ -17,31 +20,22 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    eager
     resize_to_fit(200, 200)
-    cloudinary_transformation quality: 80
   end
 
   version :medium do
-    eager
     resize_to_fit(400, 400)
-    cloudinary_transformation quality: 80
   end
 
   version :large do
-    eager
     resize_to_fit(800, 800)
-    cloudinary_transformation quality: 80
   end
 
   version :big_large do
-    eager
     resize_to_fit(1280, 720)
-    cloudinary_transformation quality: 80
   end
 
   version :gray do
     process resize_to_fill: [100, 150, :north]
-    cloudinary_transformation effect: 'grayscale'
   end
 end
