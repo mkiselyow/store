442988e2a6dc058edded0fd511a31d4f3a2549b7
MaximKiselyow <m.kiselyow@gmail.com>
2018-05-16 12:46:14 +0300

    add set cache before filter to action controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,2 +1,8 @@
 class Admin::ApplicationController < ActionController::Base
+  before_filter :set_cache
+  
+  def set_cache
+    response.headers["Cache-Control"] ="public, max-age=#{10.days.seconds.to_i}"
+    response.headers["Expires"] = "#{10.days.from_now.to_formatted_s(:rfc822)}"
+  end
 end
